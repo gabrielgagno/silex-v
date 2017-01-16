4c5510663be3cabc2f6ee2f008e65742e06f27ae
gjpgagno <gjpgagno@gmail.com>
2017-01-16 16:28:35 +0800

    added a baseModel file

---
+++ src/App/Models/BaseModel.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: gjpgagno
+ * Date: 1/16/17
+ * Time: 11:29 AM
+ */
+
+namespace App\Models;
+
+
+class BaseModel
+{
+    private $nameSpace;
+    private $modelName;
+    private $tableName;
+
+    public function __construct($modelName, $tableName = null)
+    {
+        $this->nameSpace = 'App\Models';
+        $this->modelName = $modelName;
+        $this->tableName = $tableName;
+    }
+
+    public static function getAll($limit = null, $offset = null)
+    {
+
+    }
+
+    public static function getOne($id)
+    {
+
+    }
+
+    public function save() {
+
+    }
+
+    public function create() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+
+    }
+}
